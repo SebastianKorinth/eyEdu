94e85e0776aff83bec8ad8a36cd4f776cf29eb72
SebastianKorinth <korinth@korinth.org>
2018-08-03 12:20:28 +0200

    shiny makes me crazy

--- inst/shiny-examples/eyEduShinyAoiRectangle/global.R
+++ inst/shiny-examples/eyEduShinyAoiRectangle/global.R
@@ -12,7 +12,7 @@ dim.y <- one.image.dimension[1]
 # allows scaling of the plot shown within the shiny app
 # depends on individual screen size, adjust scale.var to enlarge or to decrease
 # if scale.var = 1 100 %
-# scale.var = 0.65
+scale.var = scale.var
 plot.dim.width <- paste(as.integer(dim.x * scale.var),"px", sep = "")
 plot.dim.height <- paste(as.integer(dim.y * scale.var),"px", sep = "")
 
