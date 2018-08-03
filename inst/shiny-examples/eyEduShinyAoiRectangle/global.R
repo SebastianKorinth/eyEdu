215fbdbb538a3fe09c24b34bdd2c066d251be166
SebastianKorinth <korinth@korinth.org>
2018-08-03 12:35:31 +0200

    uff

--- inst/shiny-examples/eyEduShinyAoiRectangle/global.R
+++ inst/shiny-examples/eyEduShinyAoiRectangle/global.R
@@ -1,5 +1,3 @@
-library(png) # for opening png files
-
 
 # reads the first image from the folder, which is used to set plot dimensions
 # for all images + also needed to calculate pixel-click-location
@@ -12,7 +10,7 @@ dim.y <- one.image.dimension[1]
 # allows scaling of the plot shown within the shiny app
 # depends on individual screen size, adjust scale.var to enlarge or to decrease
 # if scale.var = 1 100 %
-scale.var = scale.var
+scale.var <- scale.var
 plot.dim.width <- paste(as.integer(dim.x * scale.var),"px", sep = "")
 plot.dim.height <- paste(as.integer(dim.y * scale.var),"px", sep = "")
 
