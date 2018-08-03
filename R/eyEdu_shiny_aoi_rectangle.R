feea8471b7dea100ac2b9fb8855ce8c1252c1d6d
SebastianKorinth <korinth@korinth.org>
2018-08-03 12:53:20 +0200

    giving it up

--- R/eyEdu_shiny_aoi_rectangle.R
+++ R/eyEdu_shiny_aoi_rectangle.R
@@ -1,10 +1,10 @@
 # function to call the shiny app for free hand aoi definition
 
 
-EyEduShinyAoiRectangle  <- function(scale.var = 0.65) {
+EyEduShinyAoiRectangle  <- function() {
   appDir <- system.file("shiny-examples", "eyEduShinyAoiRectangle", package = "eyEdu")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing eyEdu.", call. = FALSE)
+    stop("Could not find the eyEdu directory. Try re-installing eyEdu.", call. = FALSE)
   }
   
   shiny::runApp(appDir, display.mode = "normal", launch.browser = T)
