99658b66e5b3c9d623d041f7310b1f0c8ab6904c
SebastianKorinth <korinth@korinth.org>
2018-10-26 15:36:40 +0200

    added option for Dejan's search experiment, switched to http instead of https

--- R/eyEdu_get_examples.R
+++ R/eyEdu_get_examples.R
@@ -1,13 +1,24 @@
 EyEduGetExamples <- function(experiment.type = "reading"){
-  
-  
+
+
 if(experiment.type == "reading"){
 
-download.file( url = "https://github.com/SebastianKorinth/exampleDataReadingExperiment/archive/master.zip",
+download.file( url = "http://github.com/SebastianKorinth/exampleDataReadingExperiment/archive/master.zip",
                destfile = "example.zip")
 
 unzip( zipfile = "example.zip" )
 file.remove("example.zip")
+}
 
-}
-}
+  if(experiment.type == "search"){
+
+    download.file( url = "http://github.com/DejanDraschkow/exampleDataSearchExperiment/archive/master.zip",
+                   destfile = "example.zip")
+
+    unzip( zipfile = "example.zip" )
+    file.remove("example.zip")
+
+  }
+
+
+  }
