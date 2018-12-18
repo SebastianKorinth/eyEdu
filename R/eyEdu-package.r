e2807f57a430b873b6ee4b748d01ffcef88861c7
SebastianKorinth <korinth@korinth.org>
2018-12-18 17:31:44 +0100

    further documentation

---
+++ R/eyEdu-package.r
@@ -0,0 +1,10 @@
+#' eyEdu.
+#'
+#'EyEdu stands for eye-movement analysis for educational purposes.
+#'The package is NOT intended to replace any proper analysis software for eye-tracking data. It
+#'is rather intended to provide some basic functions that may help students who make their first
+#'steps into eye-tracking to get a better understanding of the method. Example experiments and data
+#'recorded using a low-cost eye-tracker (EyeTribe) are provided.
+#' @name eyEdu
+#' @docType package
+NULL
