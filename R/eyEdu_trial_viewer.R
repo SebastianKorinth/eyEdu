bada3118beb46382b159ba8d9733cdd0ff79235d
Sebastian Korinth <korinth@korinth.og>
2020-01-03 22:51:24 +0100

    added launch function for eyEdu viewer

---
+++ R/eyEdu_trial_viewer.R
@@ -0,0 +1,13 @@
+# function to call the shiny app for free hand aoi definition
+
+eyEduTrialViewer  <- function() {
+  appDir <- system.file("shiny-examples", 
+                        "eyEduTrialViewer", 
+                        package = "eyEdu")
+  if (appDir == "") {
+    stop("Could not find the eyEdu directory. Try re-installing eyEdu.", 
+         call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal", launch.browser = FALSE)
+}
