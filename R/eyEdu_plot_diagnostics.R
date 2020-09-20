ee2d0830714199b96df996caeb696fc3b60eaa15
SebastianKorinth <korinth@korinth.org>
2020-09-20 18:56:08 +0200

    added option to visualize changes on y-axis for diagnostic plots

--- R/eyEdu_plot_diagnostics.R
+++ R/eyEdu_plot_diagnostics.R
@@ -2,13 +2,15 @@ EyEduPlotDiagnostics <- function(participant.nr = NA,
                                  participant.name = NA, 
                                  trial.nr, 
                                  sample.length = 2000,
-                                 show.filtered = FALSE){
+                                 show.filtered = FALSE,
+                                 show.y.direction = FALSE){
 
 
+# not sure whether the if statement below is doing, what it is intended to do
 # checks if eyEdu.data is loaded already, if not will be loaded
   if(!exists("eyEdu.data")) {
     load(file = paste(raw.data.path, "eyEdu_data.Rda", sep = "")) 
-  }
+ }
 
   # Two options: either participant name or participant number can be provided 
   if (is.na(participant.name)) {
@@ -25,13 +27,14 @@ EyEduPlotDiagnostics <- function(participant.nr = NA,
 # Exception handler, if fixation detection was not conducted yet    
   if (is.null(eyEdu.data$participants[[list.entry.nr]]$fixation.data)) {
     return(paste("No fixations to display!",
-           "Please run fixation detectionfirst: EyEduDetectFixationsIDT()!"))
+           "Please run fixation detection first: EyEduDetectFixationsIDT()!"))
       }  
 
   if (show.filtered == TRUE) { 
     
     if(is.null(eyEdu.data$participants[[list.entry.nr]]$sample.data$x.filt)){
-      return("No filtered data available. Please run EyEduLowPassFilter() or set show.filtered = FALSE")
+      return("No filtered data available. Please run EyEduLowPassFilter() or EyEduSmooth() or
+             set show.filtered = FALSE")
     } 
     
     
@@ -41,10 +44,18 @@ plot.sample.posx <- eyEdu.data$participants[[
   list.entry.nr]]$sample.data$x.filt[eyEdu.data$participants[[
     list.entry.nr]]$sample.data$trial.index == trial.nr]
 
+plot.sample.posy <- eyEdu.data$participants[[
+  list.entry.nr]]$sample.data$y.filt[eyEdu.data$participants[[
+    list.entry.nr]]$sample.data$trial.index == trial.nr]
+
   }else {
     # Extracts sample data for participant.nr and trial.nr
     plot.sample.posx <- eyEdu.data$participants[[
       list.entry.nr]]$sample.data$rawx[eyEdu.data$participants[[
+        list.entry.nr]]$sample.data$trial.index == trial.nr]
+    
+    plot.sample.posy <- eyEdu.data$participants[[
+      list.entry.nr]]$sample.data$rawy[eyEdu.data$participants[[
         list.entry.nr]]$sample.data$trial.index == trial.nr]
     
 }
@@ -59,7 +70,7 @@ set.zero.var <- plot.sample.time[1]
 plot.sample.time <- plot.sample.time - set.zero.var
 
 # ggplot2 works only with data frames 
-plot.sample.df <- data.frame(plot.sample.time, plot.sample.posx)
+plot.sample.df <- data.frame(plot.sample.time, plot.sample.posx, plot.sample.posy)
 
 # Defines the row index, which corresponds to the highest time point below 
 # the sample length
@@ -69,6 +80,10 @@ end.point.sample <- as.numeric(max((which(
 # Extractes fixation data for participant.nr and trial.nr
 plot.fixation.posx <- eyEdu.data$participants[[
   list.entry.nr]]$fixation.data$fix.pos.x[eyEdu.data$participants[[
+    list.entry.nr]]$fixation.data$trial.index == trial.nr]
+
+plot.fixation.posy <- eyEdu.data$participants[[
+  list.entry.nr]]$fixation.data$fix.pos.y[eyEdu.data$participants[[
     list.entry.nr]]$fixation.data$trial.index == trial.nr]
 
 plot.fixation.start.time <- eyEdu.data$participants[[
@@ -82,7 +97,8 @@ plot.fixation.stop.time <- eyEdu.data$participants[[
 # ggplot2 works just with data frames   
 plot.fixation.df <- data.frame(plot.fixation.start.time, 
                                plot.fixation.stop.time, 
-                               plot.fixation.posx)
+                               plot.fixation.posx,
+                               plot.fixation.posy)
 
 # Defines the row index, which corresponds to the highest time point below 
 # the sample.length
@@ -90,7 +106,9 @@ end.point.fixation <- as.numeric(max(which(
   plot.fixation.df$plot.fixation.stop.time < sample.length)))
 
 
-# The plot itself 
+# The plots 
+if(show.y.direction == FALSE){
+
 diagnostic.plot <- suppressWarnings(ggplot() +
 geom_path(data= plot.sample.df[1:end.point.sample,],mapping = aes(
   x=plot.sample.df$plot.sample.time[1:end.point.sample], 
@@ -111,6 +129,38 @@ labs(title = paste("participant #",
                      "/ trial:", trial.nr), x="time in ms",
                      y="x direction pixels") +
 theme(plot.title = element_text(hjust = 0.5)))
+
+
+}else{
+  
+  # quick fix, I know that it is ugly
+  max.y = max(max(plot.sample.df$plot.sample.posy[1:end.point.sample], na.rm = T),
+  (max(plot.sample.df$plot.sample.posx[1:end.point.sample], na.rm = T)))
+  
+  diagnostic.plot <- suppressWarnings(ggplot() +
+                                        geom_path(data= plot.sample.df[1:end.point.sample,],mapping = aes(
+                                          x=plot.sample.df$plot.sample.time[1:end.point.sample], 
+                                          y=plot.sample.df$plot.sample.posx[1:end.point.sample]),na.rm=TRUE) +
+                                        geom_path(data= plot.sample.df[1:end.point.sample,],mapping = aes(
+                                          x=plot.sample.df$plot.sample.time[1:end.point.sample], 
+                                          y=plot.sample.df$plot.sample.posy[1:end.point.sample]),na.rm=TRUE, color = "red") +                                       
+                                        geom_rect(data=plot.fixation.df[1:end.point.fixation,], mapping=aes(
+                                          xmin=plot.fixation.df$plot.fixation.start.time[1:end.point.fixation], 
+                                          xmax=plot.fixation.df$plot.fixation.stop.time[1:end.point.fixation], 
+                                          ymin=0, ymax=max.y +20), 
+                                          color='cornflowerblue', alpha=0.2) +
+                                        
+                                        labs(title = paste("participant #", 
+                                                           eyEdu.data$participants[[
+                                                             list.entry.nr]]$header.info$participant.nr, " / ",
+                                                           eyEdu.data$participants[[
+                                                             list.entry.nr]]$header.info$participant.name, 
+                                                           "/ trial:", trial.nr), x="time in ms",
+                                             y="x and y direction pixels") +
+                                        theme(plot.title = element_text(hjust = 0.5)))
+  
+  
+}
 
 return(diagnostic.plot)
 
