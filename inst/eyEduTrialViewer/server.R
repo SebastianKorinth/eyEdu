00e27773e4802da5bf8a6007b3c458c4be3ebadd
SebastianKorinth <korinth@korinth.org>
2021-03-25 14:28:57 +0100

    Nuuuuu

--- inst/eyEduTrialViewer/server.R
+++ inst/eyEduTrialViewer/server.R
@@ -148,7 +148,13 @@ server <- function(input, output, session) {
   }) 
 
 # ends the session (in RStudio) if the browser tab is closed
-session$onSessionEnded(rm(eyEdu.data))
-session$onSessionEnded(stopApp)
+
+session$onSessionEnded(function() {
+
+    rm(eyEdu.data)
+
+  })
+
+#session$onSessionEnded(stopApp)
 
 }
