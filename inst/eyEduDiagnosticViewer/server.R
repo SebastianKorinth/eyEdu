78c879da907cb86e4c600081cb5131f9e381a4fd
SebastianKorinth <korinth@korinth.org>
2021-03-31 16:49:17 +0200

    fixes step variable in diagnostic viewer app

--- inst/eyEduDiagnosticViewer/server.R
+++ inst/eyEduDiagnosticViewer/server.R
@@ -1,6 +1,6 @@
 
 server <- function(input, output) {
-  
+
   # subset samples participant and trial
   trial.samples.fn <-
     reactive({
@@ -8,20 +8,20 @@ server <- function(input, output) {
         plot.sample.posx <- eyEdu.data$participants[[
         input$participant.name]]$sample.data$rawx[eyEdu.data$participants[[
           input$participant.name]]$sample.data$trial.index == input$trial.number]
-        
+
         plot.sample.posy <- eyEdu.data$participants[[
           input$participant.name]]$sample.data$rawy[eyEdu.data$participants[[
             input$participant.name]]$sample.data$trial.index == input$trial.number]
-      
+
         plot.sample.posx.filt <- eyEdu.data$participants[[
           input$participant.name]]$sample.data$x.filt[eyEdu.data$participants[[
             input$participant.name]]$sample.data$trial.index == input$trial.number]
-        
+
         plot.sample.posy.filt <- eyEdu.data$participants[[
           input$participant.name]]$sample.data$y.filt[eyEdu.data$participants[[
             input$participant.name]]$sample.data$trial.index == input$trial.number]
-        
-        
+
+
       # Extracts time line data for participant.nr and trial.nr
       plot.sample.time <- eyEdu.data$participants[[
         input$participant.name]]$sample.data$time[eyEdu.data$participants[[
@@ -29,29 +29,29 @@ server <- function(input, output) {
       # Zero aligning time scale
       set.zero.var <- plot.sample.time[1]
       plot.sample.time <- plot.sample.time - set.zero.var
-      
-      # ggplot2 works only with data frames 
-      plot.sample.df <- data.frame(plot.sample.time, 
-                                   plot.sample.posx, 
-                                   plot.sample.posy, 
-                                   plot.sample.posx.filt, 
+
+      # ggplot2 works only with data frames
+      plot.sample.df <- data.frame(plot.sample.time,
+                                   plot.sample.posx,
+                                   plot.sample.posy,
+                                   plot.sample.posx.filt,
                                    plot.sample.posy.filt)
-      
-      # Defines the row index, which corresponds to the highest time point below 
+
+      # Defines the row index, which corresponds to the highest time point below
       # the sample length
       end.point.sample <- as.numeric(suppressWarnings(max((which(
         plot.sample.df$plot.sample.time < input$sample.length)))))
       plot.sample.df <- plot.sample.df[1:end.point.sample, ]
-      
+
       if (show.y.axis.fn() == 0) {
         plot.sample.df$plot.sample.posy.filt <- -1
         plot.sample.df$plot.sample.posy <- -1
-        
+
       }
-      
+
       return(plot.sample.df)
     })
-  
+
 
   # subset fixations (by participant and trial index)
   trial.fixaxtions.fn <- reactive({
@@ -59,7 +59,7 @@ server <- function(input, output) {
     plot.fixation.posx <- eyEdu.data$participants[[
       input$participant.name]]$fixation.data$fix.pos.x[eyEdu.data$participants[[
         input$participant.name]]$fixation.data$trial.index ==  input$trial.number]
-   
+
     # Extracts time line data for participant.nr and trial.nr
     plot.sample.time <- eyEdu.data$participants[[
       input$participant.name]]$sample.data$time[eyEdu.data$participants[[
@@ -67,58 +67,58 @@ server <- function(input, output) {
     # Zero aligning time scale
     set.zero.var <- plot.sample.time[1]
     plot.sample.time <- plot.sample.time - set.zero.var
-     
+
     plot.fixation.start.time <- eyEdu.data$participants[[
       input$participant.name]]$fixation.data$fix.start[eyEdu.data$participants[[
         input$participant.name]]$fixation.data$trial.index == input$trial.number] - set.zero.var
-    
+
     plot.fixation.stop.time <- eyEdu.data$participants[[
       input$participant.name]]$fixation.data$fix.end[eyEdu.data$participants[[
-        input$participant.name]]$fixation.data$trial.index == input$trial.number] - set.zero.var  
-    
-    # ggplot2 works just with data frames   
-    plot.fixation.df <- data.frame(plot.fixation.start.time, 
-                                   plot.fixation.stop.time, 
+        input$participant.name]]$fixation.data$trial.index == input$trial.number] - set.zero.var
+
+    # ggplot2 works just with data frames
+    plot.fixation.df <- data.frame(plot.fixation.start.time,
+                                   plot.fixation.stop.time,
                                    plot.fixation.posx)
-    
-    # Defines the row index, which corresponds to the highest time point below 
+
+    # Defines the row index, which corresponds to the highest time point below
     # the sample.length
     end.point.fixation <- as.numeric(max(which(
       plot.fixation.df$plot.fixation.stop.time < input$sample.length)))
-    
+
     plot.fixation.df <- plot.fixation.df[1:end.point.fixation,]
     return(plot.fixation.df)
-    
-    
+
+
     })
-    
+
   output$interaction_numericInput <- renderUI({
     numericInput("trial.number", "Trial index:",
                  1,
-                 min = 1, 
+                 min = 1,
                  max = suppressWarnings(max(eyEdu.data$participants[[
                    input$participant.name]]$sample.data$trial.index)),
-                 step = 0.5 )# step doesn't work for some strange reason (steps of 2)
+                 step = 1 )# step doesn't always work for some strange reason (steps of 2)
   })
-  
-
-  
 
 
-  
-  
+
+
+
+
+
   #######################
 
   show.y.axis.fn <- reactive({if(input$checkbox.y == TRUE){y.axis <- 1}
-    else{y.axis <- 0} }) 
+    else{y.axis <- 0} })
   show.raw.fn <- reactive({if(input$checkbox.raw == TRUE){raw.val <- 1}
-    else{raw.val <- 0} }) 
+    else{raw.val <- 0} })
   show.filt.fn <- reactive({if(input$checkbox.filt == TRUE){filt.val <- 1}
-    else{filt.val <- 0} }) 
+    else{filt.val <- 0} })
 
 
 
-###############################################################################    
+###############################################################################
   # renders plot
   output$plot.image <- renderPlot({
 
@@ -171,15 +171,15 @@ server <- function(input, output) {
                                na.rm = TRUE) + 20),
                     color = 'grey', alpha = 0.2) +
       labs(x = "time in ms", y = "pixels") +
-      theme_gray() 
+      theme_gray()
       })
-  
-  ###############################################################################    
-  # ends the app upon botton press  
+
+  ###############################################################################
+  # ends the app upon botton press
   observeEvent(input$ending, {
-    
+
     stopApp()
-  }) 
-  
+  })
+
   # ends the session (in RStudio) if the browser tab is closed
 }
