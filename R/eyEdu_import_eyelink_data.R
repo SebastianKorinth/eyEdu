56d1404cfc0e974fef4609a20495cb393a433bc2
Sebastian P. Korinth <korinth@korinth.org>
2024-04-19 11:52:32 +0200

    second attempt to fix strange behavior when importing  messages of eyelink data

--- R/eyEdu_import_eyelink_data.R
+++ R/eyEdu_import_eyelink_data.R
@@ -98,7 +98,7 @@ raw.data <- read.csv(paste(raw.data.path,asc.path, raw.file.list[file.counter],
                      strip.white=T, stringsAsFactors=FALSE)
 colnames(raw.data)[1] <- "time"
 
-message.data <- raw.data[which(grepl(^"MSG", raw.data$time,useBytes = TRUE) == TRUE),]
+message.data <- raw.data[which(grepl("^MSG", raw.data$time,useBytes = TRUE) == TRUE),]
 message.data$time <- gsub("MSG", "", message.data$time)
 message.data$time <- as.numeric(message.data$time)
 colnames(message.data)[2:ncol(message.data)] <- paste("message.", 1:(ncol(message.data)-1), sep = "")
